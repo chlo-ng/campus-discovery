62e1c696a79b2c1635492deace2ce887a0a55c34
Lin, Sophia Zeng <sophialin@gatech.edu>
2022-10-15 16:20:16 -0400

    Remove comments from events page

--- frontend/pages/events.tsx
+++ frontend/pages/events.tsx
@@ -5,15 +5,8 @@ import NavBar from "../components/NavBar"
 import styles from '../styles/Events.module.css'
 
 const Events: NextPage = () => {
-    /*
-    router is used to navigate from page to page
-    use router.push("<url>") to navigate to another page
-    use router.replace("<url>") to navigate to another page and remove previous page from history
-    use router.replace("<folderName>") to navigate to the index.tsx file in a folder. look at templateFolder for example
-    */
     const router = useRouter()
 
-    // return html page you want to render
     return (
       <div>
         <Head>
