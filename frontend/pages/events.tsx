b2314953dca77d3b229292a57acce025d412ef39
slin393 <sophialin@gatech.edu>
2022-10-15 15:07:21 -0400

    Fix styling

--- frontend/pages/events.tsx
+++ frontend/pages/events.tsx
@@ -52,9 +52,9 @@ const Events: NextPage = () => {
             </div>
             <div className={styles.eventsContainer}>
               <p className={styles.header}>Browse Events</p>
-              <div className={`${styles.searchBar} input-group rounded`}>
-                <input type="search" className="form-control rounded" placeholder="Start browsing..." aria-label="Search" aria-describedby="search-addon" size={50} />
-                <span className={`${styles.searchIcon} input-group-text border-0`} id="search-addon">
+              <div className={styles.searchBar}>
+                <input type="search" className="form-control rounded" placeholder="Start browsing..." aria-label="Search" aria-describedby="search-addon" size={70} />
+                <span className={styles.searchIcon} id="search-addon">
                   <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                   </svg>
