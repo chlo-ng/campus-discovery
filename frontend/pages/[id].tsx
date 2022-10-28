10c49fc2ef2183fa5a6261a1be90bcfc7b024829
Xnan6 <xnan6@gatech.edu>
2022-10-28 02:43:46 -0400

    fix
    my backend is not running properly cant really see the result but this should fix most things

--- frontend/pages/[id].tsx
+++ frontend/pages/[id].tsx
@@ -8,21 +8,24 @@ import styles from '../styles/Events.module.css'
 
 const Post = () => {
     const router = useRouter()
-    const { event } = router.query
+    const { id } = router.query
     const [name, setName] = useState('')
     const [date, setDate] = useState('')
     const [time, setTime] = useState('')
     const [location, setLocation] = useState('')
     const [describe, setDescribe] = useState('')
     const [image, setImage] = useState('bookmark.png')
+    const date1 = new Date(date);
+    const hour = new Date(time);
     
-    fetch("http://localhost:8080/api/events/" + event).then((response) => {
+    fetch("http://localhost:8080/api/events/" + id).then((response) => {
           response.json().then((res) => {
             setName(res["title"])
             setDate(res["date"])
             setTime(res["startTime"])
             setDescribe(res["description"])
             setLocation(res["location"])
+            setImage(res["self"])
           })
         });
 
@@ -37,17 +40,20 @@ const Post = () => {
           
           
         </Head>
+          <link rel="icon" href="/gtLogo.png" />
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto' />
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto Slab' />
         <main>
           <NavBar />
           <div className="container">
             <div className={styles.topbox}>
                 <img className={styles.image} src = "/exampleEvent.png"></img>
-                <h2 className= {styles.topname}>{name}<br></br><p className = {styles.text}>{date}<br></br>{time} PM<br></br>{location}</p></h2> 
+                <h2 className= {styles.topname}>{name}<br></br><p className = {styles.text}>{date1.getDate()}<br></br>{hour.getHours()} PM<br></br>{location}</p></h2> 
                 
                 <img className= {styles.icon} src = {image} onClick={bookmarkHandler}></img>
           </div>
             <div className={styles.bottombox}>
-                <img className={styles.line} src = "/line.jpg"></img>
+                <img className={styles.line} src = "/divider.png"></img>
                 <p className={styles.info}>Event Description: <br></br></p>
                 <p className={styles.subtext}>{describe}</p>
                 <p className={styles.info}>RSVP: Check-in at event site</p>
