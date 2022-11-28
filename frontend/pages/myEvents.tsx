212073ef80e67edcd665599acf2af1f33b2bc025
slin393 <sophialin@gatech.edu>
2022-11-27 23:20:03 -0500

    Filtering and sorting events

--- frontend/pages/myEvents.tsx
+++ frontend/pages/myEvents.tsx
@@ -4,8 +4,33 @@ import Head from 'next/head'
 import NavBar from "../components/NavBar"
 import styles from "../styles/myEvents.module.css"
 import React, {useState} from 'react'
+import { eventNames } from "process"
 
 const myEvents: NextPage = () => {
+
+    var userID = null
+    if (typeof localStorage !== 'undefined') {
+      userID = localStorage.getItem("id")
+    }
+    const [events, setEvents] = useState([])
+
+    if (events.length == 0 && userID !== null) {
+      fetch("http://localhost:8080/api/users/" + userID + "/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      }).then((response) => {
+        response.json().then((res) => {
+          setEvents(res.rsvp
+            .filter(rsvp => rsvp.rsvp == "YES")
+            .sort((a: any, b: any) => {
+              return Date.parse(a.event.date + "T" + a.event.startTime + "Z") - Date.parse(b.event.date + "T" + b.event.startTime + "Z")
+            })
+            .map(event => event.event))
+        }).catch(err => alert("Couldn't get user's events."))
+      })
+    }
     
     return (
       <div>
@@ -21,6 +46,12 @@ const myEvents: NextPage = () => {
           <div className="container">
             <div className={styles.coreContainer}>
               <p className={styles.heading}>My Events</p>
+              <div>
+                {events.map((event: any) => {
+                  return (<p style={{color: events.filter(eventSearch => Date.parse(event.date + "T" + event.startTime + "Z") == Date.parse(eventSearch.date + "T" + eventSearch.startTime + "Z"))
+                      .length > 1 ? 'red' : 'black'}}>{event.title}</p>)
+                })}
+              </div>
             </div>
           </div>
         </main>
